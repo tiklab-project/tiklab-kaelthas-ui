4d4db2715f4a366806ab5beb2276541d60f76e8d
userName <userEmail>
2024-02-28 19:51:40 +0800

    前端的下拉处理,面包屑等,完成

--- src/monitorIng/monitoringDetails/components/MonitoringDetails.js
+++ src/monitorIng/monitoringDetails/components/MonitoringDetails.js
@@ -1,16 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import "./MonitoringDetails.scss"
 import {withRouter} from "react-router-dom";
-import {DatePicker, Form, Input, Pagination, Table} from "antd";
+import {Breadcrumb, DatePicker, Form, Input, Pagination, Select, Table} from "antd";
 import monitoringDetailsStore from "../store/MonitoringDetailsStore";
+import left from "../../../assets/svg/left.svg"
 
 const MonitoringDetails = (props) => {
 
     const [dataList, setDataList] = useState([]);
 
-    const {findMonitorForHost, setSearchCondition, searchCondition, total} = monitoringDetailsStore;
+    const {findMonitorForHost, setSearchCondition, searchCondition, total,findMonitorByCategories} = monitoringDetailsStore;
 
-    const { RangePicker } = DatePicker;
+    const [monitorDataSubclass, setMonitorDataSubclass] = useState([]);
+
+    const {RangePicker} = DatePicker;
+
+    const {Option} = Select;
 
     useEffect(async () => {
 
@@ -41,20 +46,14 @@ const MonitoringDetails = (props) => {
             title: '监控小类',
             dataIndex: 'dataSubclass',
             key: 'dataSubclass',
-            render: (dataSubclass) => {
-                let config = {
-                    1: "CPU详情信息",
-                    5: "磁盘使用率",
-                    6: "CPU利用率",
-                    10: "内存使用百分比"
-                }
-                return config[dataSubclass];
-            }
+
         },
         {
             title: '数据',
             dataIndex: 'reportData',
             key: 'reportData',
+            render: (text, record) => (
+                <span>{record.reportType === 1 ? `${record.reportData}%` : record.reportData}</span>)
         },
         {
             title: '上报时间',
@@ -77,9 +76,7 @@ const MonitoringDetails = (props) => {
     }
 
     async function searchByName(event) {
-        setSearchCondition({
-            monitorName: event.target.value
-        })
+
         const newVar = await findMonitorForHost();
 
         setDataList([...newVar])
@@ -102,24 +99,119 @@ const MonitoringDetails = (props) => {
         console.log('onOk: ', value);
     };
 
+    async function searchByDataCategories(event) {
+
+        setSearchCondition({
+            dataCategories: event
+        })
+        const newVar = await findMonitorForHost();
+
+        setDataList([...newVar])
+
+        //根据监控大类查询监控小类
+        const resData = await findMonitorByCategories();
+        setMonitorDataSubclass([...resData])
+    }
+
+    async function searchByDataSubclass(event) {
+
+        const newVar = await findMonitorForHost();
+
+        setDataList([...newVar])
+    }
+
+    function onchangeByDataCategories(e) {
+        setSearchCondition({
+            dataCategories: e.target.value
+        })
+    }
+
+    async function onchangeByDataSubclass(e) {
+
+        setSearchCondition({
+            dataSubclass: e
+        })
+
+        const newVar = await findMonitorForHost();
+
+        setDataList([...newVar])
+
+    }
+
+    function onchangeByName(e) {
+        setSearchCondition({
+            monitorName: e.target.value
+        })
+    }
+
+    function goBackHome() {
+        props.history.push(`/monitoring`);
+    }
+
     return (
         <div className="details">
             <div className="details-body">
+                {/*<div className="details-content">
+                    <div className="details-content-title">
+
+                    </div>
+                </div>*/}
                 <div className="details-alarm-table">
+                    <Breadcrumb>
+                        {/*<svg className="icon-left" aria-hidden="true">
+                            <use xlinkHref={`#icon-${left}`}></use>
+                        </svg>*/}
+                        <Breadcrumb.Item onClick={goBackHome}>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>{"主机:" + localStorage.getItem("hostName")}</Breadcrumb.Item>
+                        <Breadcrumb.Item>{"ip:" + localStorage.getItem("ip")}</Breadcrumb.Item>
+                    </Breadcrumb>
                     <div className="details-table-title">主机下监控信息</div>
                     <div className="details-search">
                         <div className="details-div">
-                            <RangePicker
-                                showTime={{
-                                    format: 'HH:mm:ss',
+                            <Select
+                                placeholder="请选择您的监控类型"
+                                onChange={searchByDataCategories}
+                                allowClear={true}
+                                style={{
+                                    width: 120,
                                 }}
-                                format="YYYY-MM-DD HH:mm:ss"
+                            >
+                                <Option value={"CPU"} key={1}>{"CPU"}</Option>
+                                <Option value={"IO"} key={2}>{"IO"}</Option>
+                                <Option value={"memory"} key={3}>{"memory"}</Option>
+                            </Select>
+                        </div>
+                        <div className="details-div">
+                            <Select
+                                placeholder="请选择您的监控类型"
+                                onChange={onchangeByDataSubclass}
+                                allowClear={true}
+                                style={{
+                                    width: 300,
+                                }}
+                            >
+                                {
+                                    monitorDataSubclass && monitorDataSubclass.map(item => (
+                                        <Option value={item.dataSubclass} key={item.id}>{item.dataSubclass}</Option>
+                                    ))
+                                }
+                            </Select>
+                        </div>
+                        <div className="details-div">
+                            <RangePicker
+                                /*showTime={{
+                                    format: 'HH:mm:ss',
+                                }}*/
+                                format="YYYY-MM-DD"
                                 onChange={onChange}
                                 onOk={onOk}
                             />
                         </div>
                         <div className="details-div">
-                            <Input placeholder="根据监控项名称进行查询" onPressEnter={(event) => searchByName(event)}/>
+                            <Input placeholder="根据监控项名称进行查询"
+                                   allowClear={true}
+                                   onChange={onchangeByName}
+                                   onPressEnter={(event) => searchByName(event)}/>
                         </div>
                     </div>
                     <div className="details-alarm-table-list">
@@ -149,5 +241,6 @@ const MonitoringDetails = (props) => {
         </div>
     );
 };
+
 
 export default withRouter(MonitoringDetails);
